ccacde8167e10f82bfb31d91c578afb65e52621c
davathrak <davathrak@gmail.com>
2021-12-02 23:22:36 +0530

    added model for custom user

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,45 @@ from django.contrib.auth.models import BaseUserManager , AbstractBaseUser
 
 # Create your models here.
 
+class MyAccountManager(BaseUserManager):
+    def crete_user(self,first_name, last_name, username, email, password=None):
+        if not email:
+            raise ValueError("Please enter Email, you hve not entered email")
+
+        if not username:
+            raise ValueError("please enter Username , you have not enterded username")
+        
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, first_name, last_name, username, email, password):
+        user = self.crete_user(
+            email = self.normalize_email(email),
+            first_name = first_name,
+            last_name = last_name,
+            username = username,
+            password = password,
+            
+
+        )
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superadmin = True
+
+        user.save(using=self._db)
+        return user
+
+        
+
 class Account(AbstractBaseUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -21,18 +60,20 @@ class Account(AbstractBaseUser):
     is_superadmin = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username','first_name','lsat_name']
+    REQUIRED_FIELDS = ['username','first_name','last_name']
 
+    objects = MyAccountManager()
 
     def __str__(self):
         return self.email
 
-    def has_perm(self, perm , object=None):
+    def has_perm(self, perm, object=None):
         return self.is_admin
 
-    def has_module_perm(self, add_label):
+    def has_module_perms(self, app_label):
         return True
-    
+
+
 
 
 
